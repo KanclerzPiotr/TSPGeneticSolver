b95f2eeb422331ebf8d752c203a563e488a3a656
Piotr Kanclerz <pkanclerz997@gmail.com>
2022-11-08 14:44:40 +0100

    Add cxxopts

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cxxopts.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
+    cxxopts::Options options("TSPGeneticSolver", "Traveling Salesman Problem solver with Genetic algorithm");
     std::cout << "Hello world\n";
     return 0;
 }
